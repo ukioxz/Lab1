2321f0bdca31734bc39f1b8b06687588e9ad3321
ukioxz <ikatember@gmail.com>
2022-02-10 22:43:39 +0200

    Add checking input values
    Checking input values in noninteractive mode

--- quaEquationFile.js
+++ quaEquationFile.js
@@ -20,13 +20,27 @@ async function getInfo() {
     if (fs.existsSync(fileName)) {
       const regexp = /-*\d+[.\d+]* -*\d+[.\d+]* -*\d+[.\d+]*/;
       const fileContent = fs.readFileSync(fileName, "utf-8");
-      const str = fileContent.match(regexp)[0];
-      const arr = str.split(" ");
+      //const str = fileContent.match(regexp)[0];
+    //  const arr = str.split(" ");
+    const arr = fileContent.split(" ");
       const numbers = arr.map(parseFloat);
 
       const a = numbers[0];
       const b = numbers[1];
       const c = numbers[2];
+      
+      if(typeof a !== 'number' || isNaN(a)){
+        console.log(`Error. Expected a valid real number, got ${a} instead`);
+        rl.close();
+      }
+      if(typeof b != 'number'|| isNaN(b)){
+        console.log(`Error. Expected a valid real number, got ${b} instead`);
+        rl.close();
+      }
+      if(typeof c != 'number'|| isNaN(c)){
+        console.log(`Error. Expected a valid real number, got ${c} instead`);
+        rl.close();
+      }
 
       let discriminant = b * b - 4 * a * c;
 
